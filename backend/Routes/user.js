dd2742a70fa95a30a1f6deb9cb54f80fda9b8219
prachi-33 <prachisaran5@gmail.com>
2025-06-03 22:11:32 +0530

    signin route done

--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -1,5 +1,6 @@
 const express=require("express");
 const router=express.Router();
+const bcrypt=require("bcrypt");
 require("dotenv").config();
 const jwt=require("jsonwebtoken");
 const zod=require("zod");
@@ -17,13 +18,13 @@ router.post("/signup",async(req,res)=>{
     const body=req.body;
     const success=signupSchema.safeParse(body);
     if(!success){
-        res.json({
+        res.status(400).json({
             msg:"Incorrect Inputs"
         })
     }
     const user=await User.findOne({userName: body.userName});
     if(user){
-        res.json({
+        res.status(409).json({
             msg:"User already exist"
         })
     }
@@ -31,7 +32,7 @@ router.post("/signup",async(req,res)=>{
         userName:body.userName,
         firstName:body.firstName,
         lastName:body.lastName,
-        password:body.password
+        password:bcrypt.hash(body.password,2)
     })
     .catch((err)=>console.log(err))
     const userId=dbUser._id;
@@ -42,10 +43,32 @@ router.post("/signup",async(req,res)=>{
     })
 
     const token=jwt.sign(userId,process.env.SECRET);
-    res.json({
+    res.status(201).json({
         msg:"User created succesfully",
         token:token
     })
+
+
+})
+const signinSchema=zod.object({
+    userName:zod.string().min(3),
+    password:zod.string()
+})
+
+router.post("/signin",async (req,res)=>{
+    const body=req.body;
+    const success =signinSchema.safeParse(req.body)
+    if(!success){
+        res.status(400).json({
+            msg:"Invalid inputs"
+        })
+    }
+    
+
+    const user=await User.findOne({userName:body.userName}) 
+    const hashed=user.password;
+    const isMatch=bcrypt.compare(req.password,hashed);
+    isMatch? res.status(200).json({msg:"Signin successful"}): res.status(400).json({msg:"Wrong Password"})
 
 
 })
