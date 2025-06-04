8612ef187d6d2449adc320df4a732df5f9eb2750
prachi-33 <prachisaran5@gmail.com>
2025-06-04 11:24:50 +0530

    middleware and account router done

---
+++ backend/Routes/account.js
@@ -0,0 +1,43 @@
+const express=require("express");
+const authMiddleware=require("../middleware");
+import {Account, Acoount} from "../db";
+const mongoose=require("mongoose");
+
+const router=express.Router();
+router.get("/balance",authMiddleware,async (req,res)=>{
+    const account= await Account.findOne({
+        userId:req.userId
+    });
+    res.status(200).json({
+        balance:account.balance
+    });
+});
+
+router.post("/transfer",authMiddleware,async (req,res)=>{
+    const session= await mongoose.startSession();
+    session.startTransaction();
+    const {amount,to}=req.body;
+    const account=await Account.findOne({userId:req.userId}).session(session);
+    if(!account || account.balance<amount){
+        await session.abortTransaction();
+        return res.status(400).json({
+            msg:"Insufficient balance"
+        })
+    }
+    const toAccount= await Account.findOne({userId:to}).session(session);
+    if(!toAccount){
+        await session.abortTransaction();
+        return res.status(400).json({
+            msg:"Invalid account"
+        })
+    }
+    await Account.updateOne({userId:req.userId},{$inc:{balance:-amount}}).session(session);
+
+    await Account.updateOne({userId:to},{$inc:{balance:amount}}).session(session);
+
+    await session.commitTransaction();
+    res.status(200).json({
+        msg:"Transfer successsful"
+    })
+})
+module.exports=router;
--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -1,11 +1,11 @@
 const express=require("express");
 const router=express.Router();
 const bcrypt=require("bcrypt");
+const authMiddleware=require("../middleware")
 require("dotenv").config();
 const jwt=require("jsonwebtoken");
 const zod=require("zod");
-import {User,Account} from "../db"
-module.exports=router;
+import {User,Account} from "../db";
 
 const signupSchema=zod.object({
     userName:zod.string().min(3),
@@ -42,7 +42,7 @@ router.post("/signup",async(req,res)=>{
         balance:1+Math.random()*10000
     })
 
-    const token=jwt.sign(userId,process.env.SECRET);
+    const token=jwt.sign(userId,process.env.JWT_SECRET);
     res.status(201).json({
         msg:"User created succesfully",
         token:token
@@ -66,9 +66,66 @@ router.post("/signin",async (req,res)=>{
     
 
     const user=await User.findOne({userName:body.userName}) 
+    
     const hashed=user.password;
     const isMatch=bcrypt.compare(req.password,hashed);
     isMatch? res.status(200).json({msg:"Signin successful"}): res.status(400).json({msg:"Wrong Password"})
 
+    if(user){
+        const token=jwt.sign(user._id,process.env.JWT_SECRET);
+        res.status(200).json({
+            token:token
+        })
+    }
+
 
 })
+
+const updateSchema=zod.object({
+    password:zod.string().optional(),
+    firstName:zod.string().optional(),
+    lastName:zod.string().optional()
+})
+router.put("/update", authMiddleware, async (req, res) => {
+    const { success } = updateBody.safeParse(req.body)
+    if (!success) {
+        res.status(411).json({
+            message: "Invalid Inputs"
+        })
+    }
+
+    await User.updateOne(req.body, {
+        id: req.userId
+    })
+
+    res.json({
+        message: "Updated successfully"
+    })
+})
+
+router.get("/bulk",authMiddleware,async (req,res)=>{
+    const filter =req.query.filter||"";
+    const users= await User.find({
+        $or:[{
+            firstName:{
+                "$regex":filter
+            }
+        },{
+            lastName:{
+                "$regex":filter
+            }
+        }]
+    })
+
+    res.json({
+        user:users.map(user=>({
+            userName:user.userName,
+            firstName:user.firstName,
+            lastName:user.lastName,
+            _id:user._id
+        }))
+    })
+})
+
+
+module.exports=router;
